3159fd1345f0ac82957905805ca8bec736914b47
admin <visualjupiter@gmail.com>
2018-01-01 00:56:23 +0900

    ディープコピーのためにlodashを追加

--- src/webpack/libs/Utility.js
+++ src/webpack/libs/Utility.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 export default {
   // 配列をシャッフル
   // 参考：https://qiita.com/artistan/items/9eb9a0fb14f4ec3a8764
   shuffle(array) {
-    const tmpArray = Object.assign([], array);
+    const tmpArray = _.cloneDeep(array);
     for (let i = 0; i < tmpArray.length; i += 1) {
       const j = Math.floor(Math.random() * (i + 1));
       const tmp = tmpArray[i];
