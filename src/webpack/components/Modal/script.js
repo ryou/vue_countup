8a03315feb27e66926f70b52aaa99c8c05f52f77
admin <admin@adminnoMacBook-Pro.local>
2017-12-31 14:39:43 +0900

    モーダルのソース整理

---
+++ src/webpack/components/Modal/script.js
@@ -0,0 +1,8 @@
+export default {
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
--- src/webpack/components/Modal/template.vue
+++ src/webpack/components/Modal/template.vue
@@ -11,47 +11,5 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    isVisible: {
-      type: Boolean,
-      default: false,
-    },
-  },
-};
-</script>
-
-<style lang="scss">
-  .Modal {
-    position: fixed;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-
-    background: rgba(#000, .6);
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    transition: opacity .4s;
-
-    &:not(.-visible) {
-      opacity: 0;
-      pointer-events: none;
-    }
-
-    &.-visible {
-      opacity: 1;
-    }
-  }
-  .Modal_body {
-    width: 400px;
-    padding: 40px;
-    box-sizing: border-box;
-
-    background: #fff;
-
-    text-align: center;
-  }
-</style>
+<script src="./script.js"></script>
+<style lang="scss" src="./style.scss"></style>
